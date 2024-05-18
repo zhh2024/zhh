ecafe092c37f6c07d8fea9fd3dbc69fbf9b99d72
tai123 <2507278523@qq.com>
2024-05-18 10:59:18 +0800

    zhh first

---
+++ src/main/java/ZhhTest.java
@@ -0,0 +1,11 @@
+/**
+ * @program: zhhTEst
+ * @description:
+ * @author: lxf
+ * @create: 2024-05-18 10:56
+ */
+public class ZhhTest {
+    public static void main(String[] args) {
+        System.out.println("zhh love lxf");
+    }
+}
